948c951a016d9d33a91da82cc2a9506cee6eab77
sandrocarlos <alessandro.wiley@gmail.com>
2024-04-13 23:06:59 -0400

    Upating Changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,12 +1,10 @@
-print("Something")
-
 import os 
 import csv
 
 # Source: Caite Green via Justin (the TA) 
-# Define the relative path to the CSV file
-csvpath = "./budget_data.csv"
-# Open the CSV file
+# Path to CSV File
+csvpath = "./Resources/budget_data.csv"
+# Opening CSV file
 with open(csvpath, 'r') as csvfile:
    csvReader = csv.reader(csvfile)
    #convert to a list and separate the header from the data
@@ -14,4 +12,21 @@ with open(csvpath, 'r') as csvfile:
    csv_header = csv_list[0]
    csv_data = csv_list[1:]
 
-   
+# Determine the count of months
+Count_of_Months = len(csv_data)
+#print(Count_of_Months)
+
+# Declare Variables
+
+Total_Months = Count_of_Months
+"""
+Total = 
+Average_Change = 
+Greatest_Increase = 
+Greatest_Decrease = 
+"""
+
+Profit_Losses = 0
+Date = 0
+
+print(Total_Months)
